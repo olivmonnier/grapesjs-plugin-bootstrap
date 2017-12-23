e54d127e5ec19ef2e0afd4a8991bff43c95cdef6
Olivier Monnier <omonnier.dev@gmail.com>
2017-12-24 00:25:50 +0100

    fix

--- src/components/grid.js
+++ src/components/grid.js
@@ -9,7 +9,7 @@ export default (editor, config = {}) => {
       defaults: Object.assign({}, defaultModel.prototype.defaults, {
         'custom-name': 'Row',
         droppable: ':not(.row)',
-        draggable: ':not(.row)'
+        draggable: true
       })
     }, {
       isComponent(el) {
@@ -26,42 +26,65 @@ export default (editor, config = {}) => {
       defaults: Object.assign({}, defaultModel.prototype.defaults, {
         "custom-name": "Column",
         droppable: ':not([class*="col-"])',
-        draggable: '.row',
+        draggable: true,
         traits: [{
-          type: 'select',
-          label: 'Viewport',
-          name: 'viewport',
-          options: ['xs', 'sm', 'md', 'lg'],
-          changeProp: 1
+          type: 'select-class',
+          options: [
+            {value: '', name: 'None'},
+            ... [1,2,3,4,5,6,7,8,9,10,11,12].map((i) => ({ value: `col-xs-${i}`, name: `${i}/12` }))
+          ],
+          label: 'XS size'
         }, {
-          type: 'select',
-          label: 'Width Column',
-          name: 'width-column',
-          options: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
-          changeProp: 1
+          type: 'select-class',
+          options: [
+            {value: '', name: 'None'},
+            ... [1,2,3,4,5,6,7,8,9,10,11,12].map((i) => ({ value: `col-sm-${i}`, name: `${i}/12` }))
+          ],
+          label: 'SM size'
+        }, {
+          type: 'select-class',
+          options: [
+            {value: '', name: 'None'},
+            ... [1,2,3,4,5,6,7,8,9,10,11,12].map((i) => ({ value: `col-md-${i}`, name: `${i}/12` }))
+          ],
+          label: 'MD size'
+        }, {
+          type: 'select-class',
+          options: [
+            {value: '', name: 'None'},
+            ... [1,2,3,4,5,6,7,8,9,10,11,12].map((i) => ({ value: `col-lg-${i}`, name: `${i}/12` }))
+          ],
+          label: 'LG size'
+        }, {
+          type: 'select-class',
+          options: [
+            {value: '', name: 'None'},
+            ... [1,2,3,4,5,6,7,8,9,10,11,12].map((i) => ({ value: `col-xs-offset-${i}`, name: `${i}/12` }))
+          ],
+          label: 'XS offset'
+        }, {
+          type: 'select-class',
+          options: [
+            {value: '', name: 'None'},
+            ... [1,2,3,4,5,6,7,8,9,10,11,12].map((i) => ({ value: `col-sm-offset-${i}`, name: `${i}/12` }))
+          ],
+          label: 'SM offset'
+        }, {
+          type: 'select-class',
+          options: [
+            {value: '', name: 'None'},
+            ... [1,2,3,4,5,6,7,8,9,10,11,12].map((i) => ({ value: `col-md-offset-${i}`, name: `${i}/12` }))
+          ],
+          label: 'MD offset'
+        }, {
+          type: 'select-class',
+          options: [
+            {value: '', name: 'None'},
+            ... [1,2,3,4,5,6,7,8,9,10,11,12].map((i) => ({ value: `col-lg-offset-${i}`, name: `${i}/12` }))
+          ],
+          label: 'LG offset'
         }],  
-      }),
-      init() {
-        this.listenTo(this, 'change:width-column', this.changeWidth);
-        this.listenTo(this, 'change:viewport', this.changeWidth);
-      },
-      changeWidth() {
-        const width = this.get("width-column") || '12';
-        const viewPort = this.get('viewport') || 'md';
-        const classes = this.get('classes');
-        const sm = this.sm.get("SelectorManager");
-
-        classes.forEach(className => {
-          if (className.id.match(/col-(xs|sm|md|lg)-\d+/)) {
-            classes.remove(className)
-          }
-        })
-
-        const class_obj = sm.add(`col-${viewPort}-${width}`);
-        classes.add(class_obj);
-
-        this.em.trigger("change:selectedComponent");
-      }
+      })
     }, {
       isComponent(el) {
         if (el && el.className && el.className.match(/col-(xs|sm|md|lg)-\d+/)) {
--- src/traits/selectClass.js
+++ src/traits/selectClass.js
@@ -7,57 +7,52 @@ export default function(editor, config = {}) {
     },
 
     onValueChange: function() {
-      const model = this.model;
-      const target = this.target;
-      const val = model.get('value');
-      const classList = model.get('options').map(opt => opt.value || opt);
-      const classes = target.get('classes');
-      const sm = target.sm.get('SelectorManager');
-
-      classes.forEach(element => {
-        if (classList.includes(element.id)) {
-          classes.remove(element);
+      let classes = this.model.get('options').map(opt => opt.value);
+      for(let i = 0; i < classes.length; i++) {
+        if(classes[i].length > 0) {
+          let classes_i_a = classes[i].split(' ');
+          for(let j = 0; j < classes_i_a.length; j++) {
+            if(classes_i_a[j].length > 0) {
+              this.target.removeClass(classes_i_a[j]);
+            }
+          }
         }
-      })
-
-      if (val && val !== '') {
-        const class_obj = sm.add(val);
-        classes.add(class_obj);
       }
-      target.em.trigger("change:selectedComponent");
+      const value = this.model.get('value');
+      if(value.length > 0 && value != 'GJS_NO_CLASS') {
+        const value_a = value.split(' ');
+        for(let i = 0; i < value_a.length; i++) {
+          this.target.addClass(value_a[i]);
+        }
+      }
+      this.target.em.trigger('change:selectedComponent');
     },
 
     getInputEl: function() {
-      if (!this.$input) {
-        const model = this.model;
-        const target = this.target;
-        const opts = model.get('options') || [];
-        this.$input = document.createElement('select');
-
-        if (opts.length) {
-          opts.forEach(opt => {
-            const $opt = document.createElement('option');
-
-            $opt.setAttribute('value', opt.value || opt);
-            $opt.textContent = opt.name || opt;
-
-            this.$input.append($opt)
-          })
+      if (!this.inputEl) {
+        let md = this.model;
+        let opts = md.get("options") || [];
+        let input = document.createElement("select");
+        let target = this.target;
+        let target_view_el = this.target.view.el;
+        for (let i = 0; i < opts.length; i++) {
+          let name = opts[i].name;
+          let value = opts[i].value;
+          if (value == "") {
+            value = "GJS_NO_CLASS";
+          } // 'GJS_NO_CLASS' represents no class--empty string does not trigger value change
+          let option = document.createElement("option");
+          option.text = name;
+          option.value = value;
+          const value_a = value.split(" ");
+          if(target_view_el.classList.contains(value)) {
+            option.setAttribute("selected", "selected");
+          }
+          input.append(option);
         }
-
-        let val = model.get('value');        
-        const name = model.get('name');
-
-        if (model.get('changeProp')) {
-          val = val || target.get(name);
-        }
-
-        if (val) {
-          this.$input.value = val;
-        }
+        this.inputEl = input;
       }
-
-      return this.$input;
+      return this.inputEl;
     }
   })
 }
